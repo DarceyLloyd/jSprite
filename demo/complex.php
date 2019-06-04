82959fa9af632c7fe4150cfded531474bcc89909
Darcey <Darcey.Lloyd@gmail.com>
2019-06-05 00:40:40 +0100

    1.3.4
    1.3.4

--- demo/complex.php
+++ demo/complex.php
@@ -6,50 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>jSprite - Darcey@aftc.io</title>
     <script src="./jsprite.min.js?v=<?php echo(rand(0,9999999)); ?>"></script>
-    <style>
-    .bd {
-        border: 1px solid #000000;
-    }
-    .p5 {
-        padding: 5px;
-    }
-    .m5 {
-        margin: 5px;
-    }
-    button {
-        cursor: pointer;
-    }
-
-
-    #demo {
-        display: flex;
-        flex-wrap: wrap;
-    }
-
-
-    #demo > div {
-        margin: 5px;
-        background: #FFCC00;
-        flex: 1;
-    }
-
-    #demo .btn-container {
-        display: flex;
-        /* flex-direction: row; */
-        /* flex-wrap: no-wrap; */
-    }
-
-    #demo .btn-container .btn-left {
-        display: inline-block;
-        width: 50%;
-
-    }
-
-    #demo .btn-container .btn-right {
-        display: inline-block;
-        width: 50%;
-    }
-    </style>
+    <link rel="stylesheet" href="styles.css">
+ 
     <script>
         // Utils
         String.prototype.replaceAll = function(search, replacement) {
@@ -171,6 +129,49 @@
     </script>
 </head>
 <body onload="init()">
+
+
+
+<h2>jSprite</h2>
+<a href="https://github.com/DarceyLloyd/jSprite" target="_blank">https://github.com/DarceyLloyd/jSprite</a>
+
+
+<h4>Quick and easy JavaScript sprites for use in your html.</h4>
+
+<hr>
+
+<b>USAGE:</b><br>
+<ul>
+<li>Set spriteSheet to the src of the sprite sheet image</li>
+<li>Set container to the ID of the html element that will contain your sprite</li>
+<li>Set no of columns the sprite sheet has</li>
+<li>Set no of rows the sprite sheet has</li>
+<li>If your sprite sheet doesn't use all the rows and columns you can specify the no of frames like so "frames:44"</li>
+<li>If your sprite is not aligned on the x axis correctly you can adjust the x offset viaw "widthOffset: -5"</li>
+<li>Set the timing of the sprite, note you can control the timings of each frame individually if you use timings and feed it an array of number matching the number of frames in your sprite.</li>
+</ul>
+
+<hr>
+
+<b>NOTE:</b><br>
+jSprite will resize the container to the dimensions it calculates the a single sprite frame to be, if you wish to adjust size of the sprite you can use transform scale via css or javascript.
+
+
+<pre>
+let mySprite = new jSprite({
+    spriteSheet: "./images/sheet1.png",
+    container: "sp1",
+    columns: 12,
+    rows: 7,
+    widthOffset: -6,
+    timing: 30,
+    // timings: [100,200,300,400,500,600],
+    repeat: true
+});
+
+</pre>
+
+
 
 
 <div id="demo" class="bd p5"></div>
